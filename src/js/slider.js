4c099d7c7a58e1db8d52cb47bd5081b43231b5f8
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2022-10-15 22:59:51 +0300

    add auto-slider to gallery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,15 @@
 $('.team__list').slick({
- nextArrow: '.btn__slick-prev',
-    prevArrow: '.btn__slick-next',
-    dots: true,
+nextArrow: '.btn__slick-prev',
+prevArrow: '.btn__slick-next',
+dots: true,
 } 
 );
+
+$('.gallery__list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+autoplaySpeed: 2500,
+nextArrow: '.btn__slick-left',
+prevArrow: '.btn__slick-right',
+});
