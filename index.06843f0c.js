163d0ec229dff0721521b24d10a2e71b7e82fb1f
Siryi-Oleksandr <108647823+Siryi-Oleksandr@users.noreply.github.com>
2022-10-20 18:20:47 +0000

    Deploying to gh-pages from @ Siryi-Oleksandr/ice-cream@44d04958a19ef546454cc31ca46319b8f330a4f7 🚀

---
+++ index.06843f0c.js
@@ -0,0 +1,2 @@
+function up(){var o=Math.max(document.body.scrollTop,document.documentElement.scrollTop);return o>0?(window.scrollBy(0,(o+1)/-10),t=setTimeout("up()",20)):clearTimeout(t),!1}
+//# sourceMappingURL=index.06843f0c.js.map
